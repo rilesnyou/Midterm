be3cb26a817d99103b9a03061ac5e63099e02468
WyattPeruski <66849810+WyattPeruski@users.noreply.github.com>
2020-08-06 14:37:12 -0400

    Add files via upload

--- src/MediaApp.java
+++ src/MediaApp.java
@@ -3,7 +3,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -16,6 +15,10 @@ public class MediaApp {
 		boolean repeat = true;
 		
 		while(repeat) { 
+			System.out.printf("Would you rather want to see a movie, or read a book: ");
+			String input = scnr.next();
+			if(input.equals("Movie")) {
+				repeat = false;
 			System.out.println("What would you like to see?");
 			System.out.println("'all' for every book in the list");
 			System.out.println("'author' for a list of authors");
@@ -25,7 +28,7 @@ public class MediaApp {
 			if (command.equals("quit")) {
 				break;
 			} else if (command.equals("all")) {
-				List<Book> things = readFile();
+				List<Book> things = readFileBook();
 				int i = 1; 
 				for (Book thing : things) {
 					System.out.println(i++ + ". " + thing);
@@ -39,8 +42,35 @@ public class MediaApp {
 			}
 				
 		}
+			else if(input.equals("Book")) {
+				repeat = false;
+				System.out.println("What would you like to see?");
+				System.out.println("'all' for every movie in the list");
+				System.out.println("'author' for a list of directors");
+				System.out.println("'title' for a title keyword");
+				System.out.println("'quit' to quit looking.");
+				String command = scnr.nextLine();
+				if (command.equals("quit")) {
+					break;
+				} else if (command.equals("all")) {
+					List<Movie> things = readFileMovie();
+					int i = 1; 
+					for (Movie thing : things) {
+						System.out.println(i++ + ". " + thing);
+					}
+				} else if (command.equals("author")) {
+					// pull list of authors
+				} else if (command.equals("title")) {
+					//pull book with typed title
+				} else {
+					System.out.println("Invalid command. Please type from the options correctly.");
+				}
+					
+			}
+			}
 		}
-	public static List<Book> readFile() {
+	
+	public static List<Book> readFileBook() {
 		try {
 			List<String> lines = Files.readAllLines(filePath);
 			List<Book> Books = new ArrayList<>();
@@ -56,19 +86,23 @@ public class MediaApp {
 			return new ArrayList<>();
 		}
 	}
-	private static Book getBook(Scanner scnr) {
-		String title = Validator.getString(scnr, "Enter a book title: ");
-		String author = Validator.getString(scnr, "Enter an author's name: ");
-		boolean onShelf = Validator.getYesNo(scnr, " ");
-		int dueDate = Validator.getInt(scnr, " ");
-		return new Book(title, author, onShelf, dueDate);
-	}
-	
-	public static void appendBookToFile(Book thing) {
-		String gotTitle = thing.getTitle();
-		String gotAuthor = thing.getAuthor();
-		boolean gotShelf = thing.getClass();
-		int gotDate = thing.getDueDate();
-		ArrayList<String> gotFile = Collections.singletonList(gotTitle, gotAuthor, gotShelf, gotDate);
+	public static List<Movie> readFileMovie(){
+		
+		try {
+			List<String> lines = Files.readAllLines(filePath);
+			List<Movie> Movies = new ArrayList<>();
+			for (String line : lines) {
+				String[] parts = line.split("~~~");
+				String country = parts[0];
+				Movies.add(new Movie());
+			}
+			
+			return Movies;
+		} catch (IOException e) {
+			System.out.println("Unable to read file.");
+			return new ArrayList<>();
+		}
+		
+		
 	}
 }
