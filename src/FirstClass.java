0b72f720fecb4a57265e70233e90e4443b966ab1
riley <riley.newton93@yahoo.com>
2020-08-06 14:45:40 -0400

    book and append

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,6 +1,11 @@
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class FirstClass {
+	
+//	private static Path filePath = Paths.get(the txt file name);
 
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
@@ -28,6 +33,21 @@ public class FirstClass {
 				
 		}
 		}
+	private static Book getBook(Scanner scnr) {
+		String title = Validator.getString(scnr, "Enter a book title: ");
+		String author = Validator.getString(scnr, "Enter an author's name: ");
+		boolean onShelf = Validator.getYesNo(scnr, " ");
+		int dueDate = Validator.getInt(scnr, " ");
+		return new Book(title, author, onShelf, dueDate);
+	}
+	
+	public static void appendBookToFile(Book thing) {
+		String gotTitle = thing.getTitle();
+		String gotAuthor = thing.getAuthor();
+		boolean gotShelf = thing.getClass();
+		int gotDate = thing.getDueDate();
+		ArrayList<String> gotFile = Collections.singletonList(gotTitle, gotAuthor, gotShelf, gotDate);
+	}
 	}
 
 
