c7195c7a159dd38eb6e9c078d166383a24a654be
riley <riley.newton93@yahoo.com>
2020-08-06 13:52:21 -0400

    main method add

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,21 +1,33 @@
+import java.util.Scanner;
 
 public class FirstClass {
 
 	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("Welcome to the Library station! Lets find you something to do!");
+		boolean repeat = true;
 		
-		System.out.println("Twice upon a time in the mystical land of Shangrila there dwelt "
-				+ "an immortal named Kai-Yun and his trusty band of ninja monkeys. They were very clever monkeys "
-				+ "indeed, known for their ingenious teamwork. Once they were challenged by a "
-				+ "swarm of seemingly unstoppable bugs. But following Kai-Yun’s unifying leadership, they paired up and mowed "
-				+ "down their foes. One-by-one every last pesky bug "
-				+ "fell at the quick hands of the monkeys.");
-		System.out.println("There are frogs in my skull"
-			+ "i ran out of coffeEe");
-		int coffee = 0;
-		int whatIWant = 1000;
-		for (coffee = 0; coffee < whatIWant; coffee ++) {
-			System.out.println("Brewing coffee");
+		while(repeat) { 
+			System.out.println("What would you like to see?");
+			System.out.println("'all' for every book in the list");
+			System.out.println("'author' for a list of authors");
+			System.out.println("'title' for a title keyword");
+			System.out.println("'quit' to quit looking.");
+			String command = scnr.nextLine();
+			if (command.equals("quit")) {
+				break;
+			} else if (command.equals("all")) {
+				// pull lists of books
+			} else if (command.equals("author")) {
+				// pull list of authors
+			} else if (command.equals("title")) {
+				//pull book with typed title
+			} else {
+				System.out.println("Invalid command. Please type from the options correctly.");
+			}
+				
+		}
 		}
 	}
 
-}
+
