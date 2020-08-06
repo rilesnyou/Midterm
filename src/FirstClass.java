1ce5cf5ab5770d697240af33eb24a4ef8bb32b80
WyattPeruski <66849810+WyattPeruski@users.noreply.github.com>
2020-08-06 10:40:35 -0400

    Add files via upload

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -3,7 +3,7 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		
-		System.out.println(" They were very clever monkeys indeed, known for their ingenious teamwork.");
+		System.out.println("Known for their ingenious teamwork.");
 
 	}
 
