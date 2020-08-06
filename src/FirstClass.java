e92f9b1bcf3e6ac22ddb7dbd7b5149164eff9e33
WyattPeruski <66849810+WyattPeruski@users.noreply.github.com>
2020-08-06 10:43:53 -0400

    Add files via upload

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -3,7 +3,12 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Known for their ingenious teamwork.");
+		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt "
+				+ "an immortal named Kai-Yun and his trusty band of ninja monkeys. They were very clever monkeys "
+				+ "indeed, known for their ingenious teamwork. Once they were challenged by a "
+				+ "swarm of seemingly unstoppable bugs. But following Kai-Yun’s unifying leadership, they paired up and mowed "
+				+ "down their foes. One-by-one every last pesky bug "
+				+ "fell at the quick hands of the monkeys.");
 
 	}
 
