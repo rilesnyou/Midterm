c0fee95ba718a8f484543b2fa89f8801a1300cc9
Doug Wozniak <1904uk@gmail.com>
2020-08-06 13:37:41 -0400

    added dueDate

--- src/Media.java
+++ src/Media.java
@@ -3,13 +3,16 @@ public abstract class Media {
 
 	private String title;
 	private boolean checkedOut;
+	private int dueDate;
 	
 	public Media() {
 		
 	}
 	
-	public Media(String title, boolean checkedOut) {
-		
+	public Media(String title, boolean checkedOut, int dueDate) {
+		this.title=title;
+		this.checkedOut=checkedOut;
+		this.dueDate=dueDate;
 	}
 	
 	
